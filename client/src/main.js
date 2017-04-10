5e6be099577eb95f676b583a353c640b10f1a8cc
fwon <fengw1325@gmail.com>
2017-04-10 16:16:55 +0800

    extract router

--- client/src/main.js
+++ client/src/main.js
@@ -1,18 +1,13 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
 import App from './App.vue'
 import store from './store'
-import VueRouter from 'vue-router'
+import router from './router'
 import * as types from './store/mutation-types'
 import _ from 'lodash'
-
-import proxyNetwork from './components/network.vue'
-import proxyRule from './components/rule.vue'
-import proxyMock from './components/mock.vue'
+import 'element-ui/lib/theme-default/index.css'
 
 //全局插件
-Vue.use(VueRouter);
 Vue.use(ElementUI);
 Vue.use({
     install (Vue, options) {
@@ -20,17 +15,6 @@ Vue.use({
         Vue.prototype.$ipc = global.ipcRenderer || {};
         Vue.prototype.$remoteApi = global.remoteApi;
     }
-});
-
-const routes = [
-    {path: '/', component: proxyNetwork},
-    {path: '/network', component: proxyNetwork},
-    {path: '/rule', component: proxyRule},
-    {path: '/mock', component: proxyMock}
-];
-
-const router = new VueRouter({
-    routes
 });
 
 new Vue({
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import proxyNetwork from '../components/network.vue'
+import proxyRule from '../components/rule.vue'
+import proxyMock from '../components/mock.vue'
 
-Vue.use(Router)
+Vue.use(Router);
+
+const routes = [
+    {path: '/', component: proxyNetwork},
+    {path: '/network', component: proxyNetwork},
+    {path: '/rule', component: proxyRule},
+    {path: '/mock', component: proxyMock}
+];
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
-})
+    routes
+});
